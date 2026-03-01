9b99d3ee76b8f7c5709c837833707eeacbd7f655
khachonphan <khachonphan25462546@gmail.com>
2026-03-01 14:44:01 +0700

    fix: import Pool

--- api/db.js
+++ api/db.js
@@ -1,3 +1,5 @@
+const { Pool } = require("pg");
+
 const config = process.env.DATABASE_URL
   ? {
       connectionString: process.env.DATABASE_URL,
@@ -12,3 +14,5 @@ const config = process.env.DATABASE_URL
     };
 
 const pool = new Pool(config);
+
+module.exports = pool;
